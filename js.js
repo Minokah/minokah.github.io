476abda1065fd2413698ae25bbd127324d3cc711
minokah <minokahh@gmail.com>
2021-05-12 02:14:07 -0400

    yeah

---
+++ js.js
@@ -0,0 +1,24 @@
+var code_quote = document.getElementById("code_quote");
+if (code_quote != null) {
+    var quote_list = [
+        { quote: "See? Red! Oh, wait... that's blood.", file: "audio/red.ogg" },
+        { quote: "Mmph mphna mprh", file: "audio/mmph.ogg" },
+        { quote: "Kept you waiting, huh?", file: "audio/waiting.ogg" },
+        { quote: "V has come to", file: "audio/v.ogg" },
+        { quote: "They played us like a damn fiddle!", file: "audio/fiddle.ogg" },
+        { quote: "A headcrab's on your face! Get it off your face, Alyx!", file: "audio/headcrab.ogg" },
+        { quote: "No, no! Careful, Lamarr! Those are quite fragile!", file: "audio/fragile.ogg" },
+    ]
+
+    const quote_sound = new Audio(); // edit: does not fix anything
+    quote_sound.play();
+
+    var picked = Math.floor(Math.random() * quote_list.length);
+    code_quote.innerHTML = quote_list[picked].quote;
+
+    code_quote.onmouseover = function () { code_quote.style.cursor = "pointer"; }
+    code_quote.onclick = function () {
+        quote_sound.src = quote_list[picked].file;
+        quote_sound.play();
+    }
+}
