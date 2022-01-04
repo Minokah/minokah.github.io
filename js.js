1eb3d89ec00ef2c4661f409e0a3ed1a3c69a187d
Brandon Luu <minokahh@gmail.com>
2022-01-04 02:34:15 -0500

    .

--- js.js
+++ js.js
@@ -3,11 +3,7 @@ if (code_quote != null) {
     var quote_list = [
         { quote: "See? Red! Oh, wait... that's blood.", file: "audio/red.ogg" },
         { quote: "Mmph mphna mprh", file: "audio/mmph.ogg" },
-        { quote: "Kept you waiting, huh?", file: "audio/waiting.ogg" },
         { quote: "V has come to", file: "audio/v.ogg" },
-        { quote: "They played us like a damn fiddle!", file: "audio/fiddle.ogg" },
-        { quote: "A headcrab's on your face! Get it off your face, Alyx!", file: "audio/headcrab.ogg" },
-        { quote: "No, no! Careful, Lamarr! Those are quite fragile!", file: "audio/fragile.ogg" },
         { quote: "I am... not interested little sun. Try again when you have become a man.", file: "audio/sun.ogg"}
     ]
 
