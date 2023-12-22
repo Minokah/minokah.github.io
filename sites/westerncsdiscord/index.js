95d75799505bb20979de364703af7372c9423da3
Brandon Luu <minokahh@gmail.com>
2023-12-22 16:28:43 -0500

    Size change

--- sites/westerncsdiscord/index.js
+++
@@ -1,9 +0,0 @@
-var num_first = document.getElementById("num_first")
-var num_second = document.getElementById("num_second")
-var num_third = document.getElementById("num_third")
-var num_fourth = document.getElementById("num_fourth")
-
-num_first.innerHTML = Object.keys(courses["first"]).length + " Course(s)"
-num_second.innerHTML = Object.keys(courses["second"]).length + " Course(s)"
-num_third.innerHTML = Object.keys(courses["third"]).length +  " Course(s)"
-num_fourth.innerHTML = Object.keys(courses["fourth"]).length + " Course(s)"
--- sites/westerncsdiscord/updated.js
+++
@@ -1,5 +0,0 @@
-var date = "09/01/2023"
-
-
-var last_update = document.getElementById("last_update")
-last_update.innerHTML = "Last Updated: " + date
